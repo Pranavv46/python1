2c8beef2f6fe65c012eec3c149cdcff0fe8a25ab
pranavv46 <vinupoojavinukumar@gmail.com>
2025-10-23 12:08:59 +0530

    new

---
+++ classwrk/demo.py
@@ -0,0 +1,17 @@
+apple_juice = 15.5
+orange_juice = 20
+grape_juice = 10.25
+
+total_volume = apple_juice + orange_juice + grape_juice
+
+print("Total volume sold:", total_volume, "liters")
+
+total_int = int(total_volume)
+print("Total volume (integer):", total_int, "liters")
+
+total_str = str(total_volume)
+print("The total volume sold today is " + total_str + " liters.")
+import random
+bonus = random.randint(5, 10)
+final_total = total_volume + bonus
+print("Final total including bonus:", final_total, "liters")
