f775a7d182a1c3b7ae27003f0b4eb284cb8bbe4d
pranavv46 <vinupoojavinukumar@gmail.com>
2025-10-26 12:31:08 +0530

    update

--- classwrk/day3.py
+++ classwrk/day3.py
@@ -11,7 +11,7 @@ is_email_valid="@" in email
 
 user_age=17
 is_age_valid= user_age >= 18
-can_login_final=has_account and email_verified and is_email_valid and is_age_vald
+can_login_final=has_account and email_verified and is_email_valid and is_age_valid
 
 print("Can login (account + verified):", can_login)
 print("Is email valid:", is_email_valid)
